15051bab97714bd92a3a2c09a400e4cadfe3d24f
Teller Wetzel <tellerwetzel@yahoo.com>
2021-06-09 14:47:13 -0600

    added DELETE routes and tested

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -85,6 +85,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tags found with id!' })
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
