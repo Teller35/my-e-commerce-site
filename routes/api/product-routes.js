da225df5484325e01197d410c107857e7b0d1073
Teller Wetzel <tellerwetzel@yahoo.com>
2021-06-09 14:26:08 -0600

    all POST routes done and tested

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,6 +32,34 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name'],
+      through: ProductTag,
+      as: 'tags'
+    }
+  ]
+  })
+  .then(dbProdData => {
+    if (!dbProdData) {
+      res.status(404).json({ message: 'No products found with id!' });
+      return;
+    }
+    res.json(dbProdData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -26,10 +26,42 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      as: 'tags',
+      through: ProductTag
+    }
+  ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tags found with id!' });
+      return;
+    }
+    res.json(dbTagData)
+  }) 
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
